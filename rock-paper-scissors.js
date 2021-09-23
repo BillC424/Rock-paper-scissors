289c2235fd70e32521d2de5f2706252ca60c8054
BillC424 <burr9783@pm.me>
2021-09-23 18:05:55 -0400

    Added toLowerCase method to make playerSelection parameters case insensitive

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,7 @@
 //create function computer play
 function computerPlay() {
     //define array that lists options
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     //create a variable that chooses one of the three strings from the array at random
         const random = Math.floor(Math.random() * choices.length);
     //return either Rock, Paper, or Scissors
@@ -14,37 +14,37 @@ function playRound(playerSelection, computerSelection) {
     //Make your function’s playerSelection parameter case-insensitive
 
     //Conditional statement for Rock vs. Paper
-    if (playerSelection === "rock" && computerSelection === "Paper" ) {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper" ) {
        return "You Lose! Paper beats Rock";
        //Conditional statement for Paper vs. Scissors
-      } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+      } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
           return "You Lose! Scissors beats Paper";
           //Conditional statement for Rock vs. Scissors
-      } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+      } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
           return "You Win! Rock beats Scissors";
           //Conditional statement for Rock vs. Rock
-      } else if (playerSelection === "rock" && computerSelection === "Rock") {
+      } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "rock") {
           return "It's a Tie! You both chose Rock";
       } //Conditional statement for Paper vs. Paper
-      else if (playerSelection === "paper" && computerSelection === "Paper") {
+      else if (playerSelection.toLowerCase() === "paper" && computerSelection === "paper") {
           return "It's a Tie! You both chose Paper";
       } //Conditional statement for Scissors vs. Scissors 
-      else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+      else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors") {
           return "It's a Tie! You both chose Paper";
       } // Conditional statment for scissors vs. rock 
-      else if (playerSelection === "scissors" && computerSelection === "Rock") {
+      else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
           return "You Lose! Rock beats Scissors.";
       } // Conditional statment for scissors vs. paper 
-      else if (playerSelection === "scissors" && computerSelection === "Paper") {
+      else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
           return "You Win! Scissors beats Paper";
       } // conditional statement for paper vs. rock 
-      else if (playerSelection === "paper" && computerSelection === "Rock") {
+      else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
           return "You Win! Paper beats Rock";
       }
 
-    //return a string that declares the winner of the round
+    //return a string that declares the winner of the roundß
 }
 
-const playerSelection = "rock";
+const playerSelection = "ROCK";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
