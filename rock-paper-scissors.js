3981ae35eee0646ecdbfe92cb97b5db1b1df84c7
BillC424 <burr9783@pm.me>
2021-09-23 01:42:26 -0400

    Fixed computerPlay() method to return a result from the rock/paper/scissors array instead of a random number

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,10 @@
 //create function computer play
-function computerplay() {
+function computerPlay() {
     //define array that lists options
     const choices = ["Rock", "Paper", "Scissors"];
     //create a variable that chooses one of the three strings from the array at random
         const random = Math.floor(Math.random() * choices.length);
     //return either Rock, Paper, or Scissors
-    return random;
+    return choices[random];
 }
+console.log(computerPlay());
